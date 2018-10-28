b60426bc9acdc798370f8f0da2481bb6853af4bc
xianglong123 <xl1391086179xl@163.com>
2018-10-28 14:44:53 +0800

    controller名字修改

--- src/main/java/com/suixingpay/controller/StudentController.java
+++ src/main/java/com/suixingpay/controller/StudentController.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 @Controller
 @RequestMapping("/student")
-public class stucontroller {
+public class StudentController {
 
     @Autowired
     StudentService studentService;
